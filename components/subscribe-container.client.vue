7cd59402985738dd1835f0f1e31be4ec76f94ed4
tekuto1437 <78407541+tekuto1437@users.noreply.github.com>
2022-12-09 14:16:27 +0200

    Add files via upload

---
+++ components/subscribe-container.client.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container">
+        <h1 class="container-title">Choose a suitable tariff</h1>
+        <div class="tariffs-container">
+            <TariffCard  
+            v-for="tariff of avalibleTariffs"
+            :title=tariff.title 
+            :price=tariff.price 
+            :isAvalible=tariff.isAvalible 
+            :diskSpace=tariff.diskSpace />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Subscribe-container',
+    data() {
+        return {
+            avalibleTariffs: [
+                {
+                    title: 'Free',
+                    price: 0.00,
+                    isAvalible: true,
+                    diskSpace: '100 Gb'
+                },
+                {
+                    title: 'Lite',
+                    price: 6.99,
+                    isAvalible: false,
+                    diskSpace: '350 Gb'
+                },
+                {
+                    title: 'Pro',
+                    price: 12.99,
+                    isAvalible: false,
+                    diskSpace: '1 TB'
+                },
+                {
+                    title: 'Ultra',
+                    price: 20.99,
+                    isAvalible: false,
+                    diskSpace: '3 TB'
+                }
+            ]
+        }
+    }
+}
+
+</script>
+
+<style>
+
+.container {
+    background-color: #484C54;
+    width: 80%;
+    height: 80%;
+    border-radius: 15px;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    padding: 3rem;
+}
+
+.container-title {
+    width: 100%;
+    height: 10%;
+    text-align: center;
+    color: white;
+}
+
+.tariffs-container {
+    width: 100%;
+    height: 90%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    place-items: center;
+}
+</style>
---
+++ components/tariff-card.client.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="card">
+        <div class="card-title-container">
+            <h1 class="card-title">{{ title }}</h1>
+        </div>
+        <div class="card-price-container">
+            <span> {{ price === 0 ? '0.00' : price }} € </span>
+            <p>{{ isAvalible ? 'for month' : '€/month' }}</p>
+        </div>
+        <div class="description-container">
+            {{ diskSpace }} <span> on disk </span>
+        </div>
+        <button class="buy-button"> {{ isAvalible ? 'Active' : `Buy ${title}` }} </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tariff-card',
+    props: {
+        title: {
+            type: String
+        },
+        price: {
+            type: Number
+        },
+        isAvalible: {
+            type: Boolean
+        },
+        diskSpace: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style>
+.card {
+    background-color: #B2AEAE;
+    height: 90%;
+    width: 90%;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow: hidden;
+    align-items: center;
+}
+
+.card-title-container {
+    background-color: #6E6E73;
+    width: 100%;
+    height: 15%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+
+.card-title {
+    color: white;
+    margin: 0 0 5px 0;
+}
+
+.card-price-container {
+    color: #4F5153;
+    text-align: center;
+    font-size: 35px;
+    margin-top: 40px;
+}
+
+.card-price-container > p {
+    margin: 0;
+    font-size: 18px;
+}
+
+.description-container {
+    color: #4F5153;
+    font-size: 20px;
+}
+
+.buy-button {
+    outline: none;
+    border-radius: 10px;
+    background-color: #7E3636;
+    border: none;
+    width: 85%;
+    height: 30px;
+    margin: 25px;
+    color: white;
+    font-size: 18px
+}
+</style>
